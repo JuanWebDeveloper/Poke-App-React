4c4c447528dcab6ca3e5eebdf7a071d9e77cfb26
Juan Vargas <juandeveloper19@gmail.com>
2022-07-18 21:02:41 -0500

    [FEAT] Estilos principales para el menu

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,3 +1,30 @@
-export const Navbar = () => {
-  return <div>Navbar</div>;
+import { Fragment } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import logo from '../../assets/images/logo.svg';
+
+export const Navbar = ({ children }) => {
+  window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    window.scrollY > 0 ? navbar.classList.add('scrolled') : navbar.classList.remove('scrolled');
+  });
+
+  return (
+    <Fragment>
+      <div className='navbar'>
+        <div className='navbar-logo'>
+          <img src={logo} alt='logo' />
+        </div>
+        <div className='navbar-menu'>
+          <NavLink className='navbar-menu_item' to='/auth/sign-in'>
+            Iniciar Sesión
+          </NavLink>
+          <NavLink className='navbar-menu_item' to='/auth/sign-up'>
+            Registrarse
+          </NavLink>
+        </div>
+      </div>
+      {children}
+    </Fragment>
+  );
 };
