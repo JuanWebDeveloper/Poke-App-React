7a0c6b88bbfaecdfec67398f735810557370d789
Juan Vargas <juandeveloper19@gmail.com>
2022-07-22 09:32:03 -0500

    [FEAT] Actualizacion en el estado de los pokemons

--- src/actions/pokemons.js
+++ src/actions/pokemons.js
@@ -14,7 +14,6 @@ export const getPokemons = () => {
               id: pokemon.url.split('/')[6],
               name: pokemon.name,
               image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split('/')[6]}.png`,
-              description: '',
             };
           })
           .sort((a, b) => a.id - b.id);
@@ -24,6 +23,40 @@ export const getPokemons = () => {
           payload: pokemons,
         });
         dispatch(stopLoading());
+      })
+      .catch((error) => {
+        console.log(error);
+        dispatch(stopLoading());
+      });
+  };
+};
+
+// Action To Get Pokemon Details.
+export const getPokemonDetails = (id) => () => {
+  return async (dispatch) => {
+    dispatch(startLoading());
+    await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        dispatch(stopLoading());
+        return {
+          id: data.id,
+          name: data.name,
+          image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png`,
+          description: data.flavor_text_entries.find((entry) => entry.language.name === 'en').flavor_text,
+          evolutionChain: data.evolution_chain.url,
+          evolutionChainId: data.evolution_chain.url.split('/')[6],
+          moves: data.moves.map((move) => move.move.name),
+          stats: data.stats.map((stat) => {
+            return {
+              name: stat.stat.name,
+              value: stat.base_stat,
+            };
+          }),
+          species: data.species.name,
+          types: data.types.map((type) => type.type.name),
+          evolution: data.species.evolves_from_species ? data.species.evolves_from_species.name : 'No Evolution',
+        };
       })
       .catch((error) => {
         console.log(error);
--- src/reducers/pokemonsReducer.js
+++ src/reducers/pokemonsReducer.js
@@ -1,18 +1,33 @@
 import { types } from '../types/types';
 
-const initialState = [
-  {
+const initialState = {
+  pokemons: [
+    {
+      id: '',
+      name: '',
+      image: '',
+    },
+  ],
+  pokemonDetails: {
     id: '',
     name: '',
     image: '',
     description: '',
+    moves: [''],
+    stats: [{ name: '', value: '' }],
+    species: '',
+    types: [''],
+    evolution: '',
   },
-];
+};
 
 export const pokemonsReducer = (state = initialState, action) => {
   switch (action.type) {
     case types.getPokemons:
-      return action.payload;
+      return {
+        ...state,
+        pokemons: action.payload,
+      };
     default:
       return state;
   }
