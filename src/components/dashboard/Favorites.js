19e2dd65a75e6dd450c20937498133b050385f3a
Juan Vargas <juandeveloper19@gmail.com>
2022-07-26 00:08:53 -0500

    [FEAT] Estilos principales de la vista de favoritos

--- src/components/dashboard/Favorites.js
+++ src/components/dashboard/Favorites.js
@@ -1,3 +1,19 @@
+import { Card } from './Card';
+
 export const Favorites = () => {
-  return <div>Favorites</div>;
+  return (
+    <div className='favorites'>
+      <div className='favorites-content'>
+        <h2 className='title capitalize'>Mis pokemons favoritos</h2>
+        <div className='favorites-content_pokemons'>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+    </div>
+  );
 };
