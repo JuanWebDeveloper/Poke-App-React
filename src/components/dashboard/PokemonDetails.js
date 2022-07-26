704bbf995d68dd73dc08bfcc2ccd24dfdcb18639
Juan Vargas <juandeveloper19@gmail.com>
2022-07-25 23:53:40 -0500

    [FEAT] Se agrego el componente de carga en la vista donde se muestra la informacion detallada de un pokemon

--- src/components/dashboard/PokemonDetails.js
+++ src/components/dashboard/PokemonDetails.js
@@ -1,12 +1,14 @@
-import { useEffect } from 'react';
+import { Fragment, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { getPokemonDetails } from '../../actions/pokemons';
+import { Loading } from '../loading/Loading';
 
 export const PokemonDetails = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
+  const { loading } = useSelector(({ ui }) => ui);
   const { pokemonDetails } = useSelector(({ pokemons }) => pokemons);
 
   useEffect(() => {
@@ -15,59 +17,67 @@ export const PokemonDetails = () => {
   }, [dispatch, id]);
 
   return (
-    <div className='pokemon-details'>
-      <div className='pokemon-details_content'>
-        <Link to='/dashboard/pokemons'>
-          <button className='btn-default'>
-            <i className='fas fa-arrow-left'></i>
-            Regresar a la lista
-          </button>
-        </Link>
-        <div className='pokemon-details_content-image'>
-          <img src={pokemonDetails.image} alt={pokemonDetails.name} />
+    <Fragment>
+      {loading ? (
+        <div className='main-loading'>
+          <Loading />
         </div>
-        <div className='pokemon-details_content-info'>
-          <h2 className='title capitalize'>{pokemonDetails.name}</h2>
+      ) : (
+        <div className='pokemon-details'>
+          <div className='pokemon-details_content'>
+            <Link to='/dashboard/pokemons'>
+              <button className='btn-default'>
+                <i className='fas fa-arrow-left'></i>
+                Regresar a la lista
+              </button>
+            </Link>
+            <div className='pokemon-details_content-image'>
+              <img src={pokemonDetails.image} alt={pokemonDetails.name} />
+            </div>
+            <div className='pokemon-details_content-info'>
+              <h2 className='title capitalize'>{pokemonDetails.name}</h2>
 
-          <div className='pokemon-details_content-info_section'>
-            <h2 className='sub-title'>Moves</h2>
-            <ul>
-              {pokemonDetails.moves.map((move) => (
-                <li key={move}>
-                  <i className='fa-solid fa-share'></i> {move}
-                </li>
-              ))}
-            </ul>
-          </div>
+              <div className='pokemon-details_content-info_section'>
+                <h2 className='sub-title'>Moves</h2>
+                <ul>
+                  {pokemonDetails.moves.map((move) => (
+                    <li key={move}>
+                      <i className='fa-solid fa-share'></i> {move}
+                    </li>
+                  ))}
+                </ul>
+              </div>
 
-          <div className='pokemon-details_content-info_section'>
-            <h2 className='sub-title'>Stats</h2>
-            <ul>
-              {pokemonDetails.stats.map((stat) => (
-                <li key={stat.name}>
-                  <i className='fa-solid fa-share'></i>
-                  {stat.name}: <span>{stat.value}</span>
-                </li>
-              ))}
-            </ul>
-          </div>
+              <div className='pokemon-details_content-info_section'>
+                <h2 className='sub-title'>Stats</h2>
+                <ul>
+                  {pokemonDetails.stats.map((stat) => (
+                    <li key={stat.name}>
+                      <i className='fa-solid fa-share'></i>
+                      {stat.name}: <span>{stat.value}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
 
-          <h2 className='sub-title'>
-            Specie: <span className='capitalize'>{pokemonDetails.specie}</span>
-          </h2>
+              <h2 className='sub-title'>
+                Specie: <span className='capitalize'>{pokemonDetails.specie}</span>
+              </h2>
 
-          <div className='pokemon-details_content-info_section'>
-            <h2 className='sub-title'>Types</h2>
-            <ul>
-              {pokemonDetails.types.map((type) => (
-                <li key={type}>
-                  <i className='fa-solid fa-share'></i> {type}
-                </li>
-              ))}
-            </ul>
+              <div className='pokemon-details_content-info_section'>
+                <h2 className='sub-title'>Types</h2>
+                <ul>
+                  {pokemonDetails.types.map((type) => (
+                    <li key={type}>
+                      <i className='fa-solid fa-share'></i> {type}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </Fragment>
   );
 };
