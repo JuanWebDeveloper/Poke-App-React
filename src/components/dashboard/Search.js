2d22cb3624a08ea4186b06be92d3e4f2573b78d8
Juan Vargas <juandeveloper19@gmail.com>
2022-07-25 22:22:19 -0500

    [FEAT] Estilos del buscador

--- src/components/dashboard/Search.js
+++ src/components/dashboard/Search.js
@@ -1,3 +1,21 @@
+import { Card } from './Card';
+
 export const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div className='search'>
+      <div className='search-content'>
+        <div className='search-content_search'>
+          <form>
+            <input type='text' placeholder='Nombre del pokemon' />
+            <button className='btn-default'>Buscar</button>
+          </form>
+        </div>
+        <div className='search-content_results'>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+    </div>
+  );
 };
